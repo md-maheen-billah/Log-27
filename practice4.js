30c654739e5ad648026a9517fd7fb4ae90dce7eb
md-maheen-billah <md.maheen.billah@icloud.com>
2024-02-22 11:13:39 +0100

    final commit

--- practice4.js
+++ practice4.js
@@ -1,8 +1,10 @@
-const numArr = (array) => Math.max(...array);
+const numArr = (a, b) => {
+  const number = [...a, ...b];
+  return Math.max(...number);
+};
 const odd = [1, 3, 5, 7, 9];
 const even = [2, 4, 6, 8];
-const numbers = [...odd, ...even];
-const maxNumber = numArr(numbers);
+const maxNumber = numArr(odd, even);
 console.log(maxNumber);
 
 // PS C:\Projects\Log-27> node practice4.js
